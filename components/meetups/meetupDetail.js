2f9e63913f5ebacd23cf79e6cd2b3990bb33b912
Rashidi-Moghadam <a.rashidi.m78@gmail.com>
2023-05-19 00:04:17 +0330

    navbar edited

--- components/meetups/meetupDetail.js
+++ components/meetups/meetupDetail.js
@@ -6,7 +6,7 @@ const MeetupDetail = (props) => {
   return (
     <section className='mx-auto p-4 '>
       <div className='max-w-[600px] mx-auto bg-white/60 rounded-md'>
-        <div className='flex  flex-col items-center mt-8 gap-4 drop-shadow-xl shadow-sm'>
+        <div className='flex  flex-col items-center mt-8 gap-4  shadow-sm'>
           <Image className='object-cover w-full' src={props.image} width={300} height={200} alt={props.title} />
           <div className='p-2 text-center flex flex-col gap-4'>
             <h1 className='font-bold'>{props.title}</h1>
